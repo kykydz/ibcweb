9448b941f1a6d6318817af105e7f5a3e821d4ebe
zkycloudz <zkyhersa@gmail.com>
2018-05-14 14:45:45 +0700

    aa

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <html>
     <head>
-        
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+
+        <tittle>IBC Home</tittle>
+
+        <link src="">
     </head>
 </html>
