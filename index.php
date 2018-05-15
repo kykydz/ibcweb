8bf537fde6fb0c855fe886a48babd3f5073767d0
zkycloudz <zkyhersa@gmail.com>
2018-05-15 11:14:36 +0700

    no plugins remove all

--- index.php
+++ index.php
@@ -1,11 +1,82 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-        <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
 
-        <tittle>IBC Home</tittle>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Welcome To | Bootstrap Based Admin Template - Material Design</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-        <link src="">
-    </head>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+
+    <!-- Bootstrap Core Css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/kustomize.css">
+    <style type="text/css">/*
+    	div { border: solid 1px black; }
+    	ul li { border: solid 1px black; }*/
+    </style>
+</head>
+	
+<body>
+
+	<nav class="navbar fixed-top navbar-dark navbar-expand-md justify-content-center flex-nowarp">
+	    <div class="navbar-brand d-flex w-50 mr-auto">
+	    	<a href="#"><img src="image/ibc.png" class="img-fluid" width="150" height="auto"></a>
+	    </div>
+	    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar3">
+	        <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="navbar-collapse collapse w-100" id="collapsingNavbar3">
+	        <ul class="navbar-nav w-100 justify-content-center">
+	            <li class="nav-item active">
+	                <a class="nav-link" href="#">Beranda</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="#">Tentang Kami</a>
+	            </li>
+	            <li class="nav-item">
+	                <a class="nav-link" href="#">Kontaks	</a>
+	            </li>
+	        </ul>
+	        <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
+	            <li class="nav-item" style="padding: 2%; width: 40%">
+	                <a class="nav-link btn btn-login" href="#">Masuk</a>
+	            </li>
+	            <li class="nav-item" style="padding: 2%; width: 40%">
+	                <a class="btn btn-reg nav-link" href="#">Daftar</a>
+	            </li>
+	        </ul>
+	    </div>
+	</nav>
+
+	<section id="secthead">
+		<div class="container">
+			<div class="konten-jumbo">
+				<div class="row">
+					<div class="col-md-6">
+						<h1>BEING IN CONTROL</h1>
+						<h1>OF YOUR <strong class="ex">FINANCES</strong></h1>
+						<p>Financial advice that give the amount to protect what's yours</p>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6" align="center" style="padding: 0 15% 0 4%">
+						<a class="btn btn-reg nav-link" href="#" >Daftar</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+
 </html>
