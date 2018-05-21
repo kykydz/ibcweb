c09187f20e1ef54278450e1af8976d0c8d1f5b74
zkycloudz <zkyhersa@gmail.com>
2018-05-21 14:37:59 +0700

    done detail event

---
+++ detailevent.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <title>Welcome To | Bootstrap Based Admin Template - Material Design</title>
+    <!-- Favicon-->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+
+    <!-- Bootstrap Core Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" >
+	<!-- Font Face Awesome -->
+
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+    <link rel="stylesheet" type="text/css" href="css/kustomize.css">
+
+</head>
+<body>
+	<?php include 'header.php'; ?>
+    <div class="klin">
+        Beranda > Klinik > Syarat dan Prosedur
+    </div>
+	<div class="container detev">
+        <div class="mywrapp shadows">
+            <div class="ev-head">
+                <img src="image/x.jpg" class="img-fluid" alt="">
+            </div>
+            <div class="ev-desc">
+                <div class="ev-tittle">JAMUR (Jalan-jalan Mudik Bareng) JKT-SOLO</div>
+                <div class="ev-cat">Transportation</div>
+                <hr>
+                <div class="ev-content">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <strong>Diselenggarakan Oleh: </strong><br>
+                            Wallezz
+                        </div>
+                        <div class="col-md-4">
+                            <strong>Tanggal dan Waktu</strong><br>
+                            8 - 9 Juni 2018, Pukul 08.30
+                        </div>
+                        <div class="col-md-4">
+                            <strong>Lokasi </strong><br>
+                            JKT- SOLO 45 Sudirman Point
+                        </div>
+                    </div>
+                </div>
+                <div class="ev-detail">
+                    <div class="row">
+                        <div class="col f1">
+                            DESKRIPSI EVENT
+                            <p>MUDIK BARENG WHITE HORSE</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col f1">
+                            SYARAT & KETENTUAN
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <p>Republik Indonesia (RI), umumnya disebut Indonesia, adalah negara di Asia Tenggara yang dilintasi garis khatulistiwa dan berada di antara benua Asia dan Australia serta antara Samudra Pasifik dan Samudra Hindia. Indonesia adalah negara kepulauan terbesar di dunia yang terdiri dari 17.504 pulau,[1] nama alternatif yang biasa dipakai adalah Nusantara.[2] Dengan populasi Hampir 270.054.853 juta jiwa pada tahun 2018,[3] Indonesia adalah negara berpenduduk terbesar keempat di dunia dan negara yang berpenduduk Muslim terbesar di dunia, dengan lebih dari 230 juta jiwa.[4] Bentuk pemerintahan Indonesia adalah republik, dengan Dewan Perwakilan Rakyat, Dewan Perwakilan Daerah dan Presiden yang dipilih secara langsung.<p>
+
+                            <p>Ibu kota negara Indonesia adalah Jakarta. Indonesia berbatasan darat dengan Malaysia di Pulau Kalimantan, dengan Papua Nugini di Pulau Papua dan dengan Timor Leste di Pulau Timor. Negara tetangga lainnya adalah Singapura, Filipina, Australia, dan wilayah persatuan Kepulauan Andaman dan Nikobar di India.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+	<?php include 'footer.php' ?>
+</body>
+</html>
